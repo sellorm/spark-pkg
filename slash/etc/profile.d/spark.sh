fd01023a619639a1bb825c5039d12ce048d59cf8
Mark Sellors <github@5vcc.com>
2017-03-09 12:34:26 +0000

    Minor updates to build procedure

--- slash/etc/profile.d/spark.sh
+++ slash/etc/profile.d/spark.sh
@@ -2,3 +2,4 @@ export SPARK_HOME=/opt/spark
 export SPARK_SERVER=
 export SPARK_HOST=
 export SPARK_MASTER=
+export PATH=/opt/spark/bin:${PATH}
---
+++ slash/opt/spark/extras/after-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# script to be run after the package is installed
+
+# add the spark user
+useradd spark
+
